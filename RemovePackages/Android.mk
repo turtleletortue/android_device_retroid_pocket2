4033c53da639add99d185d7ec1b1422f6842e754
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2020-12-29 13:21:28 -0500

    pocket2: missing keylayout and mobile data error * Add back keylayout for controller. Add back mobile data stuff to prevent mobile data error

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := AudioFX ExactCalculator Calendar Camera2 Clock Contacts Email Messaging Phone Recorder Telecom MmsService WeatherManagerService TelephonyProvider Teleservice WeatherManagerService Exchange2 Eleven
+LOCAL_OVERRIDES_PACKAGES := AudioFX ExactCalculator Calendar Camera2 Clock Contacts Email Messaging Phone Recorder WeatherManagerService Exchange2 Eleven
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
