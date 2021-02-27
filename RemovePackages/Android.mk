f327c3aba937a21d641785e691cfd01528021dcf
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2021-02-27 17:30:29 -0500

    pocket2: Remove more things * These use a decent amount of ram. From what I see, they aren't necessary. Will have to test more to see if this causes anything bad to occur

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := AudioFX ExactCalculator Calendar Camera2 Email Messaging Phone Recorder Exchange2 Eleven Contacts
+LOCAL_OVERRIDES_PACKAGES := AudioFX ExactCalculator Calendar Camera2 Email Messaging Phone Recorder Exchange2 Eleven Contacts Mms MmsService TelephonyProvider WAPPushManager TeleService Telecom
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
