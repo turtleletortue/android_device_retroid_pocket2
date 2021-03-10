5caa706235489a53360d0464aab548c83a729576
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2021-03-09 21:01:36 -0500

    pocket2: Remove hidls that seem to be unnecessary

--- hidl.mk
+++ hidl.mk
@@ -72,13 +72,14 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
 # Broadcastradio
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.1-impl \
-    android.hardware.broadcastradio@1.1-service
+#PRODUCT_PACKAGES += \
+#    android.hardware.broadcastradio@1.1-impl \
+#    android.hardware.broadcastradio@1.1-service
 
 # USB 
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0 \
-    android.hardware.usb@1.0-service.basic \
-    android.hardware.usb.gadget@1.0-impl \
-    android.hardware.usb.gadget@1.0-service
+#PRODUCT_PACKAGES += \
+#    android.hardware.usb@1.0 \
+#    android.hardware.usb@1.0-service.basic \
+#    android.hardware.usb.gadget@1.0-impl \
+#    android.hardware.usb.gadget@1.0-service
+
