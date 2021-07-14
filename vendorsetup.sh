343c51fbcc0d7098c190eb0d8109ff95357a6091
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2021-07-14 14:37:02 -0400

    pocket2: Remove unnecessary things

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,12 +18,3 @@ add_lunch_combo lineage_pocket2-userdebug
 # Eng builds
 add_lunch_combo lineage_pocket2-eng
 
-# Grab repola
-echo "Deleting old versions of repola"
-echo " "
-workingDir=$(pwd)
-rm $workingDir/device/retroid/pocket2/prebuilt/RePoLa/*.apk
-echo "Removed old version"
-echo ""
-echo "Downloading latest"
-cd device/retroid/pocket2/prebuilt/RePoLa/ && wget https://github.com/sreichholf/repola/releases/latest/download/launcher-release.apk && echo " " && echo "Success!" && cd ../../../../../
