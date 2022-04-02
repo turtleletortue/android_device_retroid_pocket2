95e6a1609fcb152d2e6513ac301947d28bc4fd28
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-04-02 09:57:21 -0400

    Revert "pocket2: Switch to AOSP audio service" * I am unable to get audio working under AOSP hidls, so use MTK ones again This reverts commit c604b028286b9407d934f0278c3c4aff4f1dc66b.

--- hidl.mk
+++ hidl.mk
@@ -26,12 +26,7 @@ PRODUCT_PACKAGES += \
 
 # Audio HAL
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-service \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    libaudiopreprocessing
 
 #    android.hardware.audio@2.0-impl \
 #    android.hardware.audio@2.0-service
