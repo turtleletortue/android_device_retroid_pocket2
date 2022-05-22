412c586f15b0aad7f9bd10913557e2af8313811f
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-05-22 19:39:05 -0400

    patches: Add l1 back and r1 forward in setupwizard

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -31,7 +31,7 @@ cd ../
 
 # SetupWizard patches (remove emergency call, set double clicking home as app switcher, resize resolution to 640x480)
 cd SetupWizard
-patch -p1 < ../../../device/retroid/pocket2/patches/packages/apps/SetupWizard/0001-Remove-Emergency-Call-Option.patch
+patch -p1 < ../../../device/retroid/pocket2/patches/packages/apps/SetupWizard/0001-Pocket2-Enhancements.patch
 cd ../
 
 # Trebuchet patches (allow package Launcher3Go to be included and add back all apps button)
