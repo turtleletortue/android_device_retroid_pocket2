84e4b3ae781399317030cd22515663a5d450a82e
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-10-06 19:18:24 -0400

    pocket2: Fix model name

--- aosp_pocket2.mk
+++ aosp_pocket2.mk
@@ -5,7 +5,7 @@ $(call inherit-product, device/retroid/pocket2/device.mk)
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := pocket2
 PRODUCT_MANUFACTURER := retroid
-PRODUCT_MODEL := Retroid Pocket 2
+PRODUCT_MODEL := Retroid_Pocket_2
 PRODUCT_NAME := aosp_pocket2
 PRODUCT_RELEASE_NAME := RetroidPocket2
 
