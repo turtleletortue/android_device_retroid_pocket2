a150d2cc6de5e901e4a5b9ea7336439eacb5dd9a
turtleletortue <46635000+turtleletortue@users.noreply.github.com>
2022-10-07 11:30:29 -0400

    pocket2: fix Lineage makefile

--- lineage.mk
+++ lineage.mk
@@ -8,9 +8,9 @@ $(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
 PRODUCT_BRAND := google
 PRODUCT_DEVICE := pocket2
 PRODUCT_MANUFACTURER := retroid
-PRODUCT_MODEL := Retroid Pocket 2
+PRODUCT_MODEL := Retroid_Pocket_2
 PRODUCT_NAME := lineage_pocket2
 PRODUCT_RELEASE_NAME := RetroidPocket2
 
 # Remove extra apps
-PRODUCT_PACKAGES += RemovePackages
+#PRODUCT_PACKAGES += RemovePackages
